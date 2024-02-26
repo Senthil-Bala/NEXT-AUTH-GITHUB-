772d62e9c52b19e48ff7c29388872cd5e037b53d
Senthil-Bala <senthilkumarbala100@gmail.com>
2024-02-26 12:38:02 +0530

    github provider added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,9 +4,7 @@ import ProfileIcon from "./ProfileIcon";
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between fixed left-0 right-0 px-8 h-14 bg-blue-500 text-white">
-      <Link className="font-bold" href={"/"}>
-        GTCoding.
-      </Link>
+     GobiToday
       <ProfileIcon />
     </nav>
   );
